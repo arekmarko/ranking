855c78553334f6df6a6f322d2bb66f9fba080997
Arek <s089012@student.tu.kielce.pl>
2021-01-16 18:30:12 +0100

    Zmieniono typ CAPTCHA

--- index.php
+++ index.php
@@ -10,14 +10,6 @@ $_SESSION['captcha'] = simple_php_captcha();
 <html>
 <head>
     <title>Pai &raquo; System rankingowy</title>
-    <style type="text/css">
-        pre {
-            border: solid 1px #bbb;
-            padding: 10px;
-            margin: 2em;
-        }
-
-    </style>
 </head>
 <body>
     <h1>
@@ -26,29 +18,29 @@ $_SESSION['captcha'] = simple_php_captcha();
     <form action="" method="POST">
         1. <input type="radio" name="vote" value="1" ><br>
         2. <input type="radio" name="vote" value="2" ><br>
-        3. <input type="radio" name="vote" value="3" onclick="getVote(this.value)"><br>
+        3. <input type="radio" name="vote" value="3" ><br>
         4. <input type="radio" name="vote" value="4" ><br>
         5. <input type="radio" name="vote" value="5" ><br>
         6. <input type="radio" name="vote" value="6" ><br>
-        
-        
-            <?php
-        echo '<img src="' . $_SESSION['captcha']['image_src'] . '" alt="CAPTCHA code">';
-        
-        ?>
     <br>
-    <input type="text" name="captcha">
+    <div class="g-recaptcha" data-sitekey="6LeVLi8aAAAAAA3PsbFeAvtUawfvCyUthlgLqN0E" data-theme="light" data-size="normal" data-image="image"></div>
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <input type="submit" name="submit" value="submit">
 <?php
-        if (isset($_POST['submit'])){
-            if ($_SESSION['captcha']['code'] == $_POST['submit']){
-                echo 'Nie jestes robotem';
-            }else {
-                echo 'Jestes robotem, blad!';
-            }
-        }
-            
-            ?>
+if (isset($_POST['submit'])) {
+    $secret = '6LeVLi8aAAAAAFZFOU3ezuOxvjqBUfzr_PWBj1Ch';
+    $response = $_POST['g-recaptcha-response'];
+    $remoteip = $_SERVER['REMOTE_ADDR'];
+    
+    $url = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$response&remoteip=$remoteip");
+    $result = json_decode($url, TRUE);
+    if ($result['success'] == 1) {
+      echo 'Nie jesteś botem';
+    }else{
+      echo 'Błędnie wypełnione pole reCAPTCHA';
+    }
+}
+?>
 </form>
 </body>
 </html>
